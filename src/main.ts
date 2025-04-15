23a87f39f9470d98eb600cbd791f203b0ea87c51
Lucas Menezes <luckmenez@gmail.com>
2025-04-15 14:05:10 -0300

    fix: habilitado cors global

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
   });
